92fb5ad9afa09a593e1dc1c2f963aef8717331a0
Nicole <153928826+thepronicole11@users.noreply.github.com>
2024-01-05 13:54:47 -0600

    Add files via upload

---
+++ lib/helpers.py
@@ -0,0 +1,37 @@
+# lib/helpers.py
+
+def helper_1():
+    print("Performing useful function#1.")
+
+
+def exit_program():
+    print("Goodbye!")
+    exit()
+
+def clear_push():
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+def smol_push():
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
